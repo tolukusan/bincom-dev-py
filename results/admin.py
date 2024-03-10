b7eb49f6459ae3848131eefad8021b9942f1d42a
tolukusan <ise.fola10@gmail.com>
2024-03-10 05:07:32 +0100

    I was finally able to

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from .models import PollingUnit, AnnouncedPuResults
 
-# Register your models here.
+class PollingUnitAdmin(admin.ModelAdmin):
+    fields = ('uniqueid',
+    'polling_unit_id',
+    'ward_id',
+    'lga_id',
+    'uniquewardid',
+    'polling_unit_number',
+    'polling_unit_name',
+    'polling_unit_description',
+    'lat',
+    'long',
+    'entered_by_user',
+    'date_entered',
+    'user_ip_address',)
+
+class AnnouncedPuResultsAdmin(admin.ModelAdmin):
+    fields = (
+        'result_id',
+    'polling_unit_uniqueid',
+    'party_abbreviation',
+    'party_score',
+    'entered_by_user',
+    'date_entered',
+    'user_ip_address',
+    )
+
+admin.site.register(PollingUnit, PollingUnitAdmin)
+admin.site.register(AnnouncedPuResults, AnnouncedPuResultsAdmin)
--- results/views.py
+++ results/views.py
@@ -1,24 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-# from django.http import HttpResponse
-from .models import State, LGA, PollingUnit, PartyResult
+from django.http import HttpResponse
+from .models import PollingUnit, AnnouncedPuResults
 
 def polling_unit_result(request):
-    states = State.objects.all()
-    lgas = PollingUnit.objects.filter(lga__state__state_name='Delta').values('lga__lga_name').distinct()
-
-    if request.method == 'POST':
-        state_name = request.POST.get('state')
-        lga_name = request.POST.get('lga')
-        polling_unit_number = request.POST.get('polling_unit')
-
-        polling_unit = get_object_or_404(
-            PollingUnit,
-            lga__state__state_name=state_name,
-            lga__lga_name=lga_name,
-            polling_unit_number=polling_unit_number
-        )
-
-        results = PartyResult.objects.filter(polling_unit=polling_unit)
-        return render(request, 'results/polling_unit_result.html', {'results': results})
-
-    return render(request, 'results/polling_unit_result.html', {'states': states, 'lgas': lgas})
+    my_results = AnnouncedPuResults.objects.all().filter(polling_unit_uniqueid=8)
+    return render(request, 'index.html', {'my_results':my_results})
