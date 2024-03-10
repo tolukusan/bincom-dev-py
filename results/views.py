3811152712b6ced50baef542a0e7317c3b55b77d
tolukusan <ise.fola10@gmail.com>
2024-03-10 05:34:07 +0100

    moved files around

--- results/views.py
+++ results/views.py
@@ -4,4 +4,4 @@ from .models import PollingUnit, AnnouncedPuResults
 
 def polling_unit_result(request):
     my_results = AnnouncedPuResults.objects.all().filter(polling_unit_uniqueid=8)
-    return render(request, 'index.html', {'my_results':my_results})
+    return render(request, 'results/index.html', {'my_results':my_results})
